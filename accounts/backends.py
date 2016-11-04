d64a75d8d73a20fb7f420422b6d2dc915d002413
jonathan <jonat7502@yahoo.com>
2016-11-04 16:39:19 +0000

    commit0411

---
+++ accounts/backends.py
@@ -0,0 +1,15 @@
+from models import User
+
+class EmailAuth(object):
+
+    def authenticate(self, email=None, password=None):
+        """
+        get an instance of User using the supplied email and check itspassword
+        """
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+
+            except User.DoesNotExist:
+            return None
